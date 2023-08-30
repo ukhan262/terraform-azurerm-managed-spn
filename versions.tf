f5811227b8e5d3f7b9044500694fccec2275c514
Umar Khan <37010344+ukhan262@users.noreply.github.com>
2023-08-29 22:42:48 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,13 @@
 terraform {
   required_providers {
-    # azurerm = {
-    #   source                = "hashicorp/azurerm"
-    #   version               = ">= 2.46.0"
-    #   # configuration_aliases = [azurerm.additional_kv]
-    # }
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = "~> 3.0"
+      # configuration_aliases = [azurerm.additional_kv]
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
   }
 }
